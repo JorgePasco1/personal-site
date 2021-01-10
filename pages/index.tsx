cf95caefe7a1c6f1e61857c62a9ef4ab3e636542
jorgepasco1 <jorgepascosoto@gmail.com>
2021-01-10 17:33:56 -0500

    finish basic structure of landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,43 @@
-import { Container, Header } from 'semantic-ui-react';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
-      <Container text>
-        <Header as="h2">Header</Header>
-        <p>
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-          commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque
-          penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-          Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
-          Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
-          aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
-          imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link
-          mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum
-          semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
-          porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante,
-          dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla
-          ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam
-          ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-          commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque
-          penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-          Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
-          Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
-          aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
-          imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link
-          mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum
-          semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
-          porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante,
-          dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla
-          ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam
-          ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
-        </p>
-      </Container>
+      <Head>
+        <title>Jorge Pasco</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="hero">
+        <div className="left-section">
+          <div className="left-section__tagline">
+            I strive to create unique digital products, through{' '}
+            <span className="main-yellow">creativity</span> and{' '}
+            <span className="main-red">engineering</span>.
+          </div>
+        </div>
+        <div className="right-section">
+          <div className="right-section__nav-bar nav-bar">
+            <a href="#" className="nav-bar__item active">
+              About
+            </a>
+            <a href="#" className="nav-bar__item">
+              Work
+            </a>
+            <a href="#" className="nav-bar__item">
+              Blog
+            </a>
+          </div>
+          <div className="right-section__profile profile">
+            <img src="/assets/profile.png"></img>
+            <div className="profile__name">Jorge Pasco</div>
+            <div className="profile__title">Software Engineer</div>
+          </div>
+          <div className="right_section__footer footer">
+            <img src="/assets/logos/linkedin.svg" alt="LinkedIn Logo" />
+            <img src="/assets/logos/github.svg" alt="Github Logo" />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
