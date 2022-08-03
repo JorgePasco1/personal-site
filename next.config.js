c784d6100b204291ed5a3d0e326854f69a22f5da
jorgepasco1 <jorgepascosoto@gmail.com>
2022-08-03 17:30:02 -0500

    add resume route

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   images: {
     domains: ['images.ctfassets.net'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/resume',
+        destination: '/assets/JorgePasco_Resume.pdf',
+        permanent: true,
+      },
+    ];
+  },
 };
